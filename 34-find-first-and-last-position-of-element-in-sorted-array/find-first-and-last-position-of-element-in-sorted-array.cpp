1fa8dedc14f15a6783415d7aa4ab82caaecd3256
Sneha Bhatia <137485763+snehabhatia04@users.noreply.github.com>
2025-09-07 16:08:02 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (86.33%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // int n=nums.size();
-        // int lb=lower_bound(nums.begin(), nums.end(), target)-nums.begin();
-        // int ub=upper_bound(nums.begin(), nums.end(), target)-nums.begin();
-        // if(lb==n || nums[lb]!=target) return {-1, -1};
-        // return {lb, ub-1};
-        int first =-1, last=-1;
         int n=nums.size();
-        for(int i=0; i<=n-1; i++){
-            if(nums[i]==target){
-                if(first==-1) first=i;
-                last=i;
-            }
-        }
-        return {first, last};
+        int lb=lower_bound(nums.begin(), nums.end(), target)-nums.begin();
+        int ub=upper_bound(nums.begin(), nums.end(), target)-nums.begin();
+        if(lb==n || nums[lb]!=target) return {-1, -1};
+        return {lb, ub-1};
+        // int first =-1, last=-1;
+        // int n=nums.size();
+        // for(int i=0; i<=n-1; i++){
+        //     if(nums[i]==target){
+        //         if(first==-1) first=i;
+        //         last=i;
+        //     }
+        // }
+        // return {first, last};
     }
 };
