f9dce2e4a3c275cfcbaa741fb8024507c757be2b
Sneha Bhatia <137485763+snehabhatia04@users.noreply.github.com>
2025-12-25 16:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.68%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,7 +2,12 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         if(s.length()!=goal.length()) return false;
-        string doubleS=s+s;
-        return doubleS.find(goal)!=string::npos;
+        for(int i = 0; i < s.size(); i++) {
+            rotate(s.begin(), s.begin() + 1, s.end());
+            if(s == goal) return true;
+        }
+        return false;
+        // string doubleS=s+s;
+        // return doubleS.find(goal)!=string::npos;
     }
 };
